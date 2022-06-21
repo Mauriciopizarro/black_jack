8d2777f0e916b60daa7fcd4870a4b0a8f4694e5a
Mauriciopizarro <pizarromauricio031096@gmail.com>
2022-06-21 17:55:41 -0300

    [MINOR] Improve player status

--- player_manager.py
+++ player_manager.py
@@ -11,8 +11,16 @@ class PlayerManager:
         self.player_1.recive_cards(cards_to_player_1)
         self.croupier.recive_cards(cards_to_player_2)
 
-    def get_current_cards(self):
+    def get_players_status(self):
         return {
-            'player': self.player_1.get_cards_values(),
-            'croupier': self.croupier.get_cards_values(),
+            'player': {
+                'name': self.player_1.name,
+                'cards': self.player_1.get_cards_values(),
+                'total_points': self.player_1.get_total_points()
+            },
+            'croupier': {
+                'name': self.croupier.name,
+                'cards': self.croupier.get_cards_values(),
+                'total_points': self.croupier.get_total_points()
+            }
         }
