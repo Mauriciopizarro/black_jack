3863070c5d90e3e915a2e8e2870267e0470c8a22
Mauriciopizarro <pizarromauricio031096@gmail.com>
2022-06-23 10:56:25 -0300

    [MINOR] Add request extra card from player

--- deck_manager.py
+++ deck_manager.py
@@ -8,17 +8,49 @@ class DeckManager:
     def __init__(self):
         self.deck = [
             Card(11),
+            Card(11),
+            Card(1),
             Card(1),
             Card(10),
             Card(10),
             Card(10),
+            Card(10),
+            Card(10),
+            Card(10),
+            Card(10),
+            Card(10),
+            Card(10),
+            Card(2),
+            Card(2),
+            Card(2),
             Card(2),
             Card(3),
+            Card(3),
+            Card(3),
+            Card(3),
+            Card(4),
+            Card(4),
+            Card(4),
             Card(4),
             Card(5),
+            Card(5),
+            Card(5),
+            Card(5),
+            Card(6),
+            Card(6),
+            Card(6),
             Card(6),
             Card(7),
+            Card(7),
+            Card(7),
+            Card(7),
             Card(8),
+            Card(8),
+            Card(8),
+            Card(8),
+            Card(9),
+            Card(9),
+            Card(9),
             Card(9),
         ]
 
@@ -26,10 +58,8 @@ class DeckManager:
 
     def get_cards(self, cards_to_return):
         cards = []
-
         for i in range(cards_to_return):
             card = self.deck.pop()
             cards.append(card)
 
         return cards
-
