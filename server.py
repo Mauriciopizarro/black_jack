aec6efea692487103a6143630542fd689ba7694e
msotelo-eb <62262326+msotelo-eb@users.noreply.github.com>
2022-07-13 19:33:45 -0300

    [MINOR] Mati conflict 2 (#19)

--- server.py
+++ server.py
@@ -14,3 +14,5 @@ app.add_url_rule('/stand', view_func=StandController.as_view('stand'))
 app.add_url_rule('/croupier_play', view_func=CroupierPlayController.as_view('croupier_play'))
 app.run(debug=True)
 
+# mati coment
+
